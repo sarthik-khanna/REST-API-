ecceb7a1349523ac273032cf0c4fc02b34d7308a
sarthik-khanna <164873613+sarthik-khanna@users.noreply.github.com>
2025-03-01 12:54:55 +0530

    Add files via upload

--- index2.js
+++ index2.js
@@ -41,22 +41,28 @@ app.get("/users",(req,res)=>{
 })
 
 app.get("/api/users",(req,res)=>{
-    console.log("i am route api",req.myUsername)
+    // always make x custom header
+res.setHeader("X-myName","sarthik khanna")
+// console.log(req.headers)
     return res.json(users);
 })
 
 app.get("/api/users/:id",(req,res)=>{
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id===id);
+    if(!user) return res.status(404).json({error : "not found"});
     return res.json(user);
 })
 app.post("/api/users",(req,res)=>{
 
     const body=req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg : "All field are required"});
+    }
     // console.log("body",body);
     users.push({...body,id:users.length+1})
 fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-    return res.json({status:"success",id:users.length})
+    return res.status(201). json({status:"success",id:users.length})
 })
 })
 
